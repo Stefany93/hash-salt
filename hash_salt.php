5eec156d9abeec1844fc6fb0d5dc1f4bae87333d
Stefany Dyulgerova <stefany.dyulgerova@gmail.com>
2014-03-01 11:00:29 +0200

    updates

--- hash_salt.php
+++ hash_salt.php
@@ -3,9 +3,10 @@
 		$password = trim($_GET['password']);
 		$password = htmlentities($password);
 		$hashed_password = hash('sha512',$password);
+		$salt = uniqid(mt_rand(), true);
 		print '<h1> Your raw password: <em>'.$password.'</em></h1>';
 		print '<h1> Hash SHA512: </h1> <em>'. $hashed_password.'</em>';
-		print '<h1> Salt: </h1> <em>'.$salt = uniqid(mt_rand(), true).'</em>';
+		print '<h1> Salt: </h1> <em>'.$salt.'</em>';
 		print '<h1> Hash & Salt together </h1> '.$hashed_password.'<em>'.$salt.'</em>';
 		print '<h1> Hash length = '.strlen($hashed_password).' Characters </h1>';
 		print '<h1>Salt length = '.strlen($salt).' Characters</h1>';
